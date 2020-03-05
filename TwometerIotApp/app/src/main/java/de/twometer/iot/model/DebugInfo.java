42a9a1836cee192a22c970ca9f68f2343968fd3f
Twometer <twometer@outlook.de>
2020-03-05 19:26:34 +0100

    Renaming and debugging

---
+++ TwometerIotApp/app/src/main/java/de/twometer/iot/model/DebugInfo.java
@@ -0,0 +1,74 @@
+package de.twometer.iot.model;
+
+public class DebugInfo {
+
+    private String name;
+    private String version;
+    private int registryTokenCount;
+    private int connectedDeviceCount;
+    private int registeredDeviceCount;
+    private String mode;
+    private String ssid;
+    private int rssi;
+    private int systemTime;
+    private int lastPingTime;
+    private boolean pairShutdownScheduled;
+
+    public DebugInfo(String name, String version, int registryTokenCount, int connectedDeviceCount, int registeredDeviceCount, String mode, String ssid, int rssi, int systemTime, int lastPingTime, boolean pairShutdownScheduled) {
+        this.name = name;
+        this.version = version;
+        this.registryTokenCount = registryTokenCount;
+        this.connectedDeviceCount = connectedDeviceCount;
+        this.registeredDeviceCount = registeredDeviceCount;
+        this.mode = mode;
+        this.ssid = ssid;
+        this.rssi = rssi;
+        this.systemTime = systemTime;
+        this.lastPingTime = lastPingTime;
+        this.pairShutdownScheduled = pairShutdownScheduled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getRegistryTokenCount() {
+        return registryTokenCount;
+    }
+
+    public int getConnectedDeviceCount() {
+        return connectedDeviceCount;
+    }
+
+    public int getRegisteredDeviceCount() {
+        return registeredDeviceCount;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getSsid() {
+        return ssid;
+    }
+
+    public int getRssi() {
+        return rssi;
+    }
+
+    public int getSystemTime() {
+        return systemTime;
+    }
+
+    public int getLastPingTime() {
+        return lastPingTime;
+    }
+
+    public boolean isPairShutdownScheduled() {
+        return pairShutdownScheduled;
+    }
+}
--- TwometerIotApp/app/src/main/java/de/twometer/iot/net/api/ApiClient.java
+++ TwometerIotApp/app/src/main/java/de/twometer/iot/net/api/ApiClient.java
@@ -5,16 +5,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.twometer.iot.model.Bridge;
+import de.twometer.iot.model.DebugInfo;
 import de.twometer.iot.model.Device;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class ApiClient {
+
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private OkHttpClient client;
 
@@ -27,9 +34,7 @@ public class ApiClient {
 
     public void loadBridgeInfo() {
         try {
-            String payload = doGet(getBridgeEndpoint(""));
-            System.out.println(payload);
-            JSONObject info = new JSONObject(payload);
+            JSONObject info = new JSONObject(doGet(getBridgeEndpoint("")));
             bridge.setName(info.getString("name"));
             bridge.setVersion(info.getString("version"));
         } catch (IOException | JSONException e) {
@@ -59,6 +64,49 @@ public class ApiClient {
         }
     }
 
+    public boolean renameDevice(Device device, String newName) {
+        return renameDevice(device.getUuid(), newName);
+    }
+
+    public boolean renameDevice(String deviceId, String newName) {
+        try {
+            return isOk(doPost(String.format("%s?id=%s&name=%s", getBridgeEndpoint("name"), urlEncode(deviceId), urlEncode(newName)), ""));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public DebugInfo getDebugInfo() {
+        try {
+            JSONObject object = new JSONObject(doGet(getBridgeEndpoint("debug")));
+            return new DebugInfo(
+                    object.getString("name"),
+                    object.getString("version"),
+                    object.getInt("registry_token_count"),
+                    object.getInt("connected_device_count"),
+                    object.getInt("registered_device_count"),
+                    object.getString("mode"),
+                    object.getString("wifi_ssid"),
+                    object.getInt("wifi_rssi"),
+                    object.getInt("system_time"),
+                    object.getInt("last_ping_stime"),
+                    object.getBoolean("pair_shutdown_sched")
+            );
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private boolean isOk(String response) {
+        try {
+            return new JSONObject(response).getString("status").equalsIgnoreCase("ok");
+        } catch (JSONException e) {
+            return false;
+        }
+    }
+
     private String getBridgeEndpoint(String ep) {
         return String.format("http://%s/%s", bridge.getIp(), ep);
     }
@@ -70,6 +118,26 @@ public class ApiClient {
 
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
+        }
+    }
+
+    private String doPost(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    private String urlEncode(String val) {
+        try {
+            return URLEncoder.encode(val, "UTF8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
