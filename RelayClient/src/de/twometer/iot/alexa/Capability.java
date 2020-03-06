b8cec11500f438117a54670ce2f216bf63c942c9
Twometer <twometer@outlook.de>
2020-03-07 00:45:46 +0100

    Alexa data structure

---
+++ RelayClient/src/de/twometer/iot/alexa/Capability.java
@@ -0,0 +1,15 @@
+package de.twometer.iot.alexa;
+
+public class Capability {
+
+    private String type;
+
+    private String _interface;
+
+    private String instance;
+
+    private String version;
+
+    // TODO properties
+
+}
---
+++ RelayClient/src/de/twometer/iot/alexa/Endpoint.java
@@ -0,0 +1,17 @@
+package de.twometer.iot.alexa;
+
+public class Endpoint {
+
+    private String endpointId;
+
+    private String manufacturerName;
+
+    private String description;
+
+    private String friendlyName;
+
+    private String[] displayCategories;
+
+    private Capability[] capabilities;
+
+}
