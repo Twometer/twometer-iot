dd01261c89e1517039d2a17b9433a9f80f949327
Twometer <twometer@outlook.de>
2020-03-23 14:03:52 +0100

    Improve transistions

--- Devices/LedController/LedController.ino
+++ Devices/LedController/LedController.ino
@@ -25,7 +25,7 @@ void setup() {
   iot.describe("Smart LED Stripe", "Twometer Electronics", "A smart, fancy and colorful LED stripe light", TYPE_LIGHT);
 
   iot.handle("Lamp.Brightness", [](const DynamicJsonDocument & payload) {
-    int brightness = payload["brightness"].as<float>() * 100.0f;
+    int brightness = payload["brightness"].as<float>();
     currentSetting.b = (float)brightness;
     hasChanged = true;
     return true;
@@ -65,11 +65,10 @@ void loop() {
     thisColor.s = currentSetting.s;
     thisColor.b = (currentPowerState ? currentSetting.b : 0.0f);
 
-    float time_step = 35; // ms
-    for (float i = 0.0f; i <= 1.0f; i += 0.1f) {
+    for (float i = 0.0f; i <= 1.0f; i += 0.01f) {
       Color color = interpolate(lastColor, thisColor, i);
       writePwm(color);
-      delay(time_step);
+      delay(4);
     }
 
     writePwm(thisColor);
@@ -79,28 +78,41 @@ void loop() {
   }
 }
 
-void writePwm(Color color) { 
+void writePwm(Color color) {
   int rgb[3];
   H2R_HSBtoRGB(color.h, color.s, color.b, rgb);
 
-  if (color.s > 20) {
+  if (color.s > 20) { // Above 20, use RGB LEDs
     analogWrite(PIN_R, remapToPwm(rgb[0]));
     analogWrite(PIN_G, remapToPwm(rgb[1]));
     analogWrite(PIN_B, remapToPwm(rgb[2]));
     analogWrite(PIN_W, 0);
-  } else {
+  } else if (color.s >= 1) { // From 1..20, use RGB and W LEDs
+    float rgbFac = color.s / 100.0f;
+    float wFac = 1.0 - rgbFac;
+    analogWrite(PIN_R, remapToPwm(rgb[0] * rgbFac));
+    analogWrite(PIN_G, remapToPwm(rgb[1] * rgbFac));
+    analogWrite(PIN_B, remapToPwm(rgb[2] * rgbFac));
+    analogWrite(PIN_W, remapToPwm(int(color.b * 2.55f * wFac)));
+  } else { // Saturation ~0 just set it to white
     analogWrite(PIN_R, 0);
     analogWrite(PIN_G, 0);
     analogWrite(PIN_B, 0);
-    analogWrite(PIN_W, remapToPwm(int(color.b * 2.55f)));
+    analogWrite(PIN_W, remapToPwm(int(color.b * 2.550f)));
   }
 }
 
 Color interpolate(Color a, Color b, float v) {
-  float ch = a.h + (b.h - a.h) * v;
+  float ch = interpolateHue(a.h, b.h, v);
   float cs = a.s + (b.s - a.s) * v;
   float cb = a.b + (b.b - a.b) * v;
   return { ch, cs, cb };
+}
+
+float interpolateHue(float h1, float h2, float v) {
+  float d = h2 - h1;
+  float delta = (d + ((abs(d) > 180.f) ? ((d < 0) ? 360.f : -360.f) : 0.f));
+  return int((h1 + (delta * v)) + 360) % 360;
 }
 
 // RGB is 0...255 but PWM is 0...1023 on ESP8266
