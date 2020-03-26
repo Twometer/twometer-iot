a0bce3992860acc825988418ad8e4b52f1cf6e65
Twometer <twometer@outlook.de>
2020-03-26 16:52:16 +0100

    Desk light controller

---
+++ Devices/DeskLampController/DeskLampController.ino
@@ -0,0 +1,33 @@
+#include <TwometerIoT.h>
+
+TwometerIoT iot;
+
+#define PIN_PWR 14
+
+int curPwm = 1023;
+
+void setup() {
+  iot.describe("Desk LEDs", "Twometer Electronics", "Desk underglow LEDs", TYPE_LIGHT);
+
+  iot.handle("Device.PowerState", [](const DynamicJsonDocument & payload) {
+    int nextPwm = payload["powerState"].as<bool>() ? 1023 : 0;
+
+    for (float i = 0.0f; i <= 1.0f; i += 0.01f) {
+      int pwm = int(curPwm + float(nextPwm - curPwm) * i);
+      analogWrite(PIN_PWR, pwm);
+      delay(4);
+    }
+    curPwm = nextPwm;
+    return true;
+  });
+
+  iot.begin();
+
+  pinMode(PIN_PWR, FUNCTION_3);
+  pinMode(PIN_PWR, OUTPUT);
+  analogWrite(PIN_PWR, curPwm);
+}
+
+void loop() {
+  iot.update();
+}
