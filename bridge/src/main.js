9cc5a87bd8c24f760966cf1813538fbdecc42af4
Twometer <twometer@outlook.de>
2021-04-18 18:12:41 +0200

    Startup error handling

--- bridge/src/main.js
+++ bridge/src/main.js
@@ -7,6 +7,5 @@ async function main() {
     logger.info(`Staring ${packageInfo.name} v${packageInfo.version}...`);
 }
 
-main().then(() => {
-    logger.success('Startup complete');
-});
+main().then(() => logger.success('Startup complete'))
+    .catch(e => logger.error(`Startup failed: ${e}`));
