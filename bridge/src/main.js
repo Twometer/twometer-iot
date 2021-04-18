86d6b77a522b610e3ae667ef1f52d133bb83e531
Twometer <twometer@outlook.de>
2021-04-19 01:18:04 +0200

    Build environment variable config loader

--- bridge/src/main.js
+++ bridge/src/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const packageInfo = require('../package.json')
+const config = require('./config')
 const logger = require('xa')
 
 async function main() {
     logger.info(`Staring ${packageInfo.name} v${packageInfo.version}...`);
+    config.load();
 }
 
 main().then(() => logger.success('Startup complete'))
