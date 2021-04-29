41ce897ad34208d68b280f0e12f502a4617c93a0
Twometer <twometer@outlook.de>
2021-04-29 18:38:06 +0200

    Fix bug when WiFi SSID had a space

--- bridge/src/core/wifi.js
+++ bridge/src/core/wifi.js
@@ -12,7 +12,7 @@ module.exports = {
         }
 
         return new Promise((resolve, reject) => {
-            let command = `/bin/bash ${__dirname}/../../scripts/wifiupdate.sh ${ssid} ${password}`
+            let command = `/bin/bash ${__dirname}/../../scripts/wifiupdate.sh "${ssid}" "${password}"`
             let child = childProcess.exec(command);
             child.on('exit', code => {
                 if (code === 0)
